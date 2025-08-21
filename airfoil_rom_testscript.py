6f5161d44d9f183768a98ad188de307d3aa695c1
ed-low <ejlowell@ucsd.edu>
2025-08-20 17:26:24 -0700

    Extending for optimization

--- airfoil_rom_testscript.py
+++ airfoil_rom_testscript.py
@@ -212,9 +212,13 @@ comm_size = comm.Get_size()
 rank_str      = f"{rank:0{len(str(comm_size-1))}d}" # string with zero-padded rank index (for prints)
 print(comm_size)
 
+# region File paths
+geometry_pickle_file_path   = Path(geometry_directory)/geometry_pickle_file_name
+stp_file_path               = Path(geometry_directory)/stp_file_name
 
 # region DAFoam instance
 dafoam_instance             = instantiateDAFoam(da_options, comm, dafoam_directory, mesh_options)
+x_surf_dafoam_initial       = dafoam_instance.getSurfaceCoordinates()
 
 # Load ROM training data
 state_store_path            = dafoam_directory/'airfoil_training/airfoil_training_point0.h5'
@@ -228,9 +232,78 @@ local_data, global_metadata = read_snapshots(state_store_path, dafoam_instance=d
 recorder = csdl.Recorder(inline=True, debug=True)
 recorder.start()
 
-# Create a volume coordinate variable
-initial_mesh_coords = dafoam_instance.xv0
-x_vol_dafoam        = csdl.Variable(value=initial_mesh_coords.ravel(order="C"), name='aero_vol_coords')
+
+geometry = lsdo_geo.import_geometry(stp_file_path,
+                                                parallelize=False)
+
+
+projected_surf_mesh_dafoam = geometry.project(
+    x_surf_dafoam_initial, 
+    grid_search_density_parameter = 1,      
+    projection_tolerance          = 1.e-3,  
+    grid_search_density_cutoff    = 50,     
+    force_reprojection            = False,
+    plot                          = False  
+)
+
+# -------------------------------------------------------------------------------------------
+# COPY PASTED GEOMETRY STUFF HERE:
+# region Create Parameterization Objects
+num_ffd_coefficients_chordwise = 5
+num_ffd_sections               = 2  # Symmetry boundaries (left, right)
+ffd_block = construct_ffd_block_around_entities(entities=geometry, 
+                                                num_coefficients=(num_ffd_coefficients_chordwise, num_ffd_sections, 2), degree=(3,1,1))
+
+# region CSDL Variable declaration
+percent_change_in_thickness          = csdl.Variable(shape=(num_ffd_coefficients_chordwise, num_ffd_sections), value=0.) # (5,2)
+percent_change_in_thickness_dof      = csdl.Variable(shape=(num_ffd_coefficients_chordwise-2,), value=np.array([0,0,0])) 
+normalized_percent_camber_change     = csdl.Variable(shape=(num_ffd_coefficients_chordwise, num_ffd_sections),  value=0.)
+normalized_percent_camber_change_dof = csdl.Variable(shape=(num_ffd_coefficients_chordwise-2,), value=np.array([0,0,0]))
+
+# ffd_block.plot()
+ffd_sectional_parameterization = VolumeSectionalParameterization(
+    name="ffd_sectional_parameterization",
+    parameterized_points=ffd_block.coefficients,    # ffd_block.coefficients.shape = (5, 2, 2, 3)
+    principal_parametric_dimension=1,
+)
+
+
+# region Evaluate Inner Parameterization Map To Define Forward Model For Parameterization Solver
+sectional_parameters = VolumeSectionalParameterizationInputs()
+ffd_coefficients     = ffd_sectional_parameterization.evaluate(sectional_parameters, plot=False)
+
+
+# Apply shape variables (NEW) : (1) THICKNESS
+original_block_thickness    = ffd_block.coefficients.value[0, 0, 1, 2] - ffd_block.coefficients.value[0, 0, 0, 2] # normal-thickness  
+percent_change_in_thickness = percent_change_in_thickness.set(csdl.slice[1:-1,0], percent_change_in_thickness_dof)
+percent_change_in_thickness = percent_change_in_thickness.set(csdl.slice[1:-1,1], percent_change_in_thickness_dof)
+delta_block_thickness       = (percent_change_in_thickness / 100) * original_block_thickness
+thickness_upper_translation = 1/2 * delta_block_thickness
+thickness_lower_translation = -thickness_upper_translation
+
+ffd_coefficients = ffd_coefficients.set(csdl.slice[:,:,1,2], ffd_coefficients[:,:,1,2] + thickness_upper_translation)
+ffd_coefficients = ffd_coefficients.set(csdl.slice[:,:,0,2], ffd_coefficients[:,:,0,2] + thickness_lower_translation)
+
+
+# Parameterize camber change as normalized by the original block (kind of like chord) length (NEW) : (2) CAMBER
+normalized_percent_camber_change = normalized_percent_camber_change.set(csdl.slice[1:-1, 0], normalized_percent_camber_change_dof)
+normalized_percent_camber_change = normalized_percent_camber_change.set(csdl.slice[1:-1, 1], normalized_percent_camber_change_dof)
+
+block_length     = ffd_block.coefficients.value[1, 0, 0, 0] - ffd_block.coefficients.value[0, 0, 0, 0]
+camber_change    = (normalized_percent_camber_change/100)*block_length
+ffd_coefficients = ffd_coefficients.set(csdl.slice[:,:,:,2], ffd_coefficients[:,:,:,2] + csdl.expand(camber_change, (num_ffd_coefficients_chordwise, num_ffd_sections, 2), 'ij->ijk'))
+
+geometry_coefficients = ffd_block.evaluate_ffd(coefficients=ffd_coefficients, plot=False)
+geometry.set_coefficients(geometry_coefficients) 
+# -------------------------------------------------------------------------------------------
+
+x_surf_dafoam = geometry.evaluate(projected_surf_mesh_dafoam, plot=False)
+x_surf_dafoam   = x_surf_dafoam.flatten()
+
+# region IDWarp and DAFoam
+idwarp_model    = DAFoamMeshWarper(dafoam_instance)
+x_vol_dafoam    = idwarp_model.evaluate(x_surf_dafoam)
+
 
 # Generate PYOFM object, and read reference mesh cell volumes
 current_dir = os.getcwd()
@@ -510,7 +583,7 @@ os.chdir(current_dir)
 
 
 # DAFoamSolver Implicit component setup and evaluation
-dafoam_rom           = DAFoamROM(dafoam_instance, pod_modes=pod_modes, tolerance=1e-10, fom_ref_state=scaling_factors*y_reference, max_iters=1000, scaling_factors=scaling_factors, weights=weights, update_jac_frequency=2)
+dafoam_rom           = DAFoamROM(dafoam_instance, pod_modes=pod_modes, tolerance=1e-10, fom_ref_state=scaling_factors*y_reference, max_iters=1000, scaling_factors=scaling_factors, weights=weights, update_jac_frequency=1)
 dafoam_rom_states    = dafoam_rom.evaluate(dafoam_input_variables_group)
 
 print(f'dafoam_rom_states: {dafoam_rom_states.value}')
@@ -523,11 +596,105 @@ dafoam_function_outputs = dafoam_functions.evaluate(dafoam_fom_states,
                                                     dafoam_input_variables_group)
 
 
+# region Optimization problem selection
+# optimization_case options
+# 1: Maximize CL/CD wrt angle-of-attack
+# 2: Minimize CD wrt angle-of-attack, wing shape (thickness/camber ffd), constrained by CL=0.5
+# 3: Minimize CL/CD wrt wing shape (thickness/camber ffd)
+optimization_case = 3
+
+
+if optimization_case == 1:
+    # Declaring and naming some variables
+    lift = dafoam_function_outputs.lift
+    drag = dafoam_function_outputs.drag
+
+    # Design variables
+    flight_conditions_group.angle_of_attack_deg.set_as_design_variable(lower=0, upper=10, scaler=1./10)
+
+    # Objectives
+    objective_fun = -lift/drag
+    objective_fun.set_as_objective()
+
+
+elif optimization_case == 2:
+    # Declaring and naming some variables
+    dynamic_pressure = 0.5*ambient_conditions_group.rho_kg_m3*flight_conditions_group.airspeed_m_s*flight_conditions_group.airspeed_m_s
+    lift = dafoam_function_outputs.lift
+    drag = dafoam_function_outputs.drag
+    CL   = lift/(dynamic_pressure*A0)
+    CD   = drag/(dynamic_pressure*A0)
+
+    # Design variables
+    flight_conditions_group.angle_of_attack_deg.set_as_design_variable(lower=0, upper=10, scaler=1./10)
+    percent_change_in_thickness_dof.set_as_design_variable(lower=-100, upper=100, scaler=1./100)
+    normalized_percent_camber_change_dof.set_as_design_variable(lower=-50, upper=50, scaler=1./50)
+
+    # Constraints
+    CL.set_as_constraint(equals=0.5)
+
+    # Objective
+    CD.set_as_objective()
+
+
+elif optimization_case == 3:
+    lift = dafoam_function_outputs.lift
+    drag = dafoam_function_outputs.drag
+
+    # Design variables
+    percent_change_in_thickness_dof.set_as_design_variable(lower=-100, upper=100, scaler=1./100)
+    normalized_percent_camber_change_dof.set_as_design_variable(lower=-50, upper=50, scaler=1./50)
+
+    # Objectives
+    objective_fun = -lift/drag
+    objective_fun.set_as_objective()
+
+
+else:
+    print('Not a valid case number')
 
 recorder.stop()
 
 
+# ===============================
+# region SIM
+# ===============================
+sim = csdl.experimental.PySimulator(recorder)
 
+# Can set design variables here and run sim to test
+# sim[root_twist]  = 3*3.14159/180
+# sim.run()
+
+# Uncomment to run and check derivatives via finite difference
+# sim.check_totals()
+# derivs = sim.compute_totals([CD],[root_twist, tip_twist, flight_conditions_group.angle_of_attack_deg])
+
+# Only allow visualization on the root rank
+if rank == 0 and not is_headless():
+    visualize_on_this_rank = True
+else:
+    visualize_on_this_rank = False
+
+# Optimization solver setup and run
+prob        = CSDLAlphaProblem(problem_name=f'{problem_name}_rank{rank_str}', simulator=sim)
+
+# # PySLSQP optimizer setup
+# solver_options = {'maxiter': 20,
+#                   'iprint': 2,
+#                   'visualize': visualize_on_this_rank,
+#                   'summary_filename': f'rank{rank_str}_slsqp_summary.out',
+#                   'save_figname':     f'rank{rank_str}_slsqp_plot.pdf',
+#                   'save_filename':    f'rank{rank_str}_slsqp_recorder.hdf5'}
+# optimizer   = PySLSQP(prob, solver_options=solver_options)
+# optimizer.solve()
+# optimizer.print_results()
+
+
+# OpenSQP optimizer setup
+open_sqp_options = {'maxiter': 20,
+                    'readable_outputs': ['x']}
+optimizer   = OpenSQP(prob, **open_sqp_options)
+optimizer.solve()
 
 
 
