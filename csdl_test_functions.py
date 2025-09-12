46391178699988133441e82fbd88d2a030bfdd85
ed-low <ejlowell@ucsd.edu>
2025-09-12 06:41:07 -0700

    Added mode option for fwd and rev mode checks.

--- csdl_test_functions.py
+++ csdl_test_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # region TEST_JACVEC_PRODUCT
-def test_jacvec_product(component, input_vals, v, w, eps=1e-6):
+def test_jacvec_product(component, input_vals, v, w, eps=1e-6, mode='rev'):
     """
     Generic test for compute_jacvec_product in a CSDL component. Tests via finite differenece
     w^T J^T v = v^T J w
@@ -48,36 +48,43 @@ def test_jacvec_product(component, input_vals, v, w, eps=1e-6):
     else:
         TypeError('the supplied component doesn''t seem to be a CSDL Implicit or Explicit component')
     
-    # --- Compute J w (forward mode, via finite diff) ---
-    #  perturb input in the w-direction
-    perturbed_input_vals = {k: np.copy(vv) for k, vv in input_vals.items()}
-    for in_name in input_vals:
-        perturbed_input_vals[in_name] += eps*w[in_name]
+    if mode == 'fwd':
+        raise NotImplementedError('forward mode has not been implemented for test_jacvec_product')
+        
+    if mode == 'rev':
+        # --- Compute J w (forward mode, via finite diff) ---
+        #  perturb input in the w-direction
+        perturbed_input_vals = {k: np.copy(vv) for k, vv in input_vals.items()}
+        for in_name in input_vals:
+            perturbed_input_vals[in_name] += eps*w[in_name]
 
-    if component_type == 'explicit':
-        perturbed_output_vals = {k: np.zeros_like(vv) for k, vv in output_vals.items()}
-        component.compute(perturbed_input_vals, perturbed_output_vals)
-        J_w = {name: (perturbed_output_vals[name] - output_vals[name]) / eps for name in output_vals.keys()}
+        if component_type == 'explicit':
+            perturbed_output_vals = {k: np.zeros_like(vv) for k, vv in output_vals.items()}
+            component.compute(perturbed_input_vals, perturbed_output_vals)
+            J_w = {name: (perturbed_output_vals[name] - output_vals[name]) / eps for name in output_vals.keys()}
+
+        else:
+            perturbed_residual_vals = {k: np.zeros_like(vv) for k, vv in residual_vals.items()}
+            component.evaluate_residuals(perturbed_input_vals, output_vals, perturbed_residual_vals)
+            J_w = {name: (perturbed_residual_vals[name] - residual_vals[name]) / eps for name in residual_vals.keys()}
+
+        rhs = sum(np.vdot(v[name], J_w[name]) for name in v)
+
+        # --- Compute J^T v (adjoint mode) ---
+        # Initialize our product to zeros (this is our J^T v)
+        JT_v    = {name: np.zeros_like(val) for name, val in input_vals.items()}
+
+        if component_type == 'explicit':   
+            component.compute_jacvec_product(input_vals, output_vals, JT_v, v, 'rev')
+
+        else:
+            d_outputs   = {name: np.zeros_like(val) for name, val in residual_vals.items()}
+            component.compute_jacvec_product(input_vals, output_vals, JT_v, d_outputs, v, 'rev')
+
+        lhs = sum(np.vdot(w[name], JT_v[name]) for name in input_vals)
 
     else:
-        perturbed_residual_vals = {k: np.zeros_like(vv) for k, vv in residual_vals.items()}
-        component.evaluate_residuals(perturbed_input_vals, output_vals, perturbed_residual_vals)
-        J_w = {name: (perturbed_residual_vals[name] - residual_vals[name]) / eps for name in residual_vals.keys()}
-
-    rhs = sum(np.vdot(v[name], J_w[name]) for name in v)
-
-    # --- Compute J^T v (adjoint mode) ---
-    # Initialize our product to zeros (this is our J^T v)
-    JT_v    = {name: np.zeros_like(val) for name, val in input_vals.items()}
-
-    if component_type == 'explicit':   
-        component.compute_jacvec_product(input_vals, output_vals, JT_v, v, 'rev')
-
-    else:
-        d_outputs   = {name: np.zeros_like(val) for name, val in residual_vals.items()}
-        component.compute_jacvec_product(input_vals, output_vals, JT_v, d_outputs, v, 'rev')
-
-    lhs = sum(np.vdot(w[name], JT_v[name]) for name in input_vals)
+        raise ValueError(f'"{mode}" not recognized. Only support "fwd" and "rev" modes') 
 
     # --- Compare ---
     err = np.abs(lhs - rhs) / (rhs)
